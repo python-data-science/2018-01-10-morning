0c9a6d95cce2f6a029330531ad89d9adca540066
Lewis <Ron.Lewis@McKesson.com>
2018-01-10 11:22:43 -0500

    here ya go - means

--- answers.py
+++ answers.py
@@ -8,20 +8,22 @@ import numpy as np
 df = pd.read_excel('Long-Term-Unemployment-Statistics.xlsx')
 df['Year']=df.Period.dt.year
 df_year = df[df.Year ==  2007]
-df_year_gender = df_year.groupby(['Gender','Year'])['Unemployed'].sum()
+df_year_gender = df_year.groupby(['Gender','Year'])['Unemployed'].mean()
 """
 Answer = Gender  Year
-         Men     2007    8471000
+         Men     2007    100845.238095
+
 """
 
 # Answer #2
 df['Month']=df.Period.dt.month
 df_women = df[df.Gender == 'Women']
-df_women_month = df_women.groupby(['Month'])['Unemployed'].sum()
+df_women_month = df_women.groupby(['Month'])['Unemployed'].mean()
 
 """
-Answer in March
-3     15485000
+Answer in February
+2     220298.701299
+
 
 
 
